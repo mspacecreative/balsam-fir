4815fc82b39e279e96b78a2046cf5c3617bfa5fd
Matt <matt@mspacecreative.com>
2017-11-09 17:42:55 -0400

    c

--- wp-content/themes/balsam-fir/functions.php
+++ wp-content/themes/balsam-fir/functions.php
@@ -5,14 +5,13 @@ function my_theme_enqueue_styles() {
 
 	$parent_style = 'parent-style';
 	
-		wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ), wp_get_theme()->get('Version')
-		);
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ));
 }
 
 /* FOOTER SCRIPTS */
-function footer_scripts() 
-{
+function footer_scripts() {
+	
 	wp_register_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
 	wp_enqueue_script('scripts');
 	
@@ -32,4 +31,4 @@ if( function_exists('acf_add_options_page') ) {
 
 /* ENQUEUE STYLES/SCRIPTS */
 add_action('init', 'footer_scripts');
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
