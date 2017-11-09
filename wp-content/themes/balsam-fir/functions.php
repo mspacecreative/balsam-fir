5fce3cbe34dfb0611324a217d2071eb03fa46cac
Matt <matt@mspacecreative.com>
2017-11-09 11:52:12 -0400

    c

--- wp-content/themes/balsam-fir/functions.php
+++ wp-content/themes/balsam-fir/functions.php
@@ -1,10 +1,11 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/* MAIN STYLESHEET */
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 }
 
+/* FOOTER SCRIPTS */
 function footer_scripts() 
 {
 	wp_register_script('scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery'), null, true);
@@ -12,11 +13,9 @@ function footer_scripts()
 	
 	wp_register_script('fontawesome', 'https://use.fontawesome.com/6ccd600e51.js', array('jquery'), null, true);
 	wp_enqueue_script('fontawesome');
-			
-	/*wp_register_script('parallax-js', get_template_directory_uri() . '/js/dzsparallaxer.js', array('jquery'), null, true);
-	wp_enqueue_script('parallax-js');*/
 }
 add_action('init', 'footer_scripts');
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 /* OPTIONS MENU */
 if( function_exists('acf_add_options_page') ) {
@@ -26,5 +25,7 @@ if( function_exists('acf_add_options_page') ) {
 	acf_add_options_sub_page('General Information');
 	acf_add_options_sub_page('Header');
 	acf_add_options_sub_page('Footer');
-	
 }
+
+/* DEREGISTER JQUERY IN HEAD
+if ( !is_admin() ) wp_deregister_script('jquery'); */
